4fce3d656d87e5a436206dd3f815f9fca73f85ef
marutanm <marutanm@gmail.com>
2012-02-24 23:00:28 +0900

    add test for Budget::update_total

--- test/models/budget_test.rb
+++ test/models/budget_test.rb
@@ -5,4 +5,15 @@ describe "Budget Model" do
     @budget = Budget.new
     refute_nil @budget
   end
+
+  it 'update_total return total' do
+    @budget = Budget.create(yearmonth: '201202')
+    @budget.payments.create(price: 100)
+    @budget.update_total
+    assert(100 == @budget.total)
+
+    @budget.payments.create(price: 20)
+    @budget.update_total
+    assert(120 == @budget.total)
+  end
 end
